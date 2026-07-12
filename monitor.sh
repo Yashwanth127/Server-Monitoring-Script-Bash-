ab45c13512291122c7b9a737d5a7df844329bd6f
Yashwanth127 <yashwanthkumarsmhy@gmail.com>
2026-07-12 22:24:42 +0530

    Initial commit

---
+++ monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+
+echo "======================================================="
+echo "            LINUX SERVER MONITOR DASHBOARD "
+echo "======================================================="
+echo
+echo "Welcome to the Server Monitoring Project!"
+echo
+echo "Project by:Yashwanth Kumar S"
+echo
